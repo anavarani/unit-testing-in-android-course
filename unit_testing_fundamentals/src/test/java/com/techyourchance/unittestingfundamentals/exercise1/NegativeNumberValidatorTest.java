ffcc74d740eb8f50bb8006e1f3e72393b71e7b4d
Ana Varani <ana.varani@booking.com>
2019-11-05 22:38:30 +0000

    [Exercise 1] Test completed

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,39 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class NegativeNumberValidatorTest {
+
+    NegativeNumberValidator SUT;
+
+    @Before
+    public void setup() {
+        SUT = new NegativeNumberValidator();
+    }
+
+    // if negative number - return true
+    @Test
+    public void isNegative_negativeNumber_trueReturned() {
+        boolean result = SUT.isNegative(-1);
+        assertThat(result, is(true));
+    }
+
+    // if positive number - return false
+    @Test
+    public void isNegative_positiveNumber_falseReturned() {
+        boolean result = SUT.isNegative(1);
+        assertThat(result, is(false));
+    }
+
+    // if zero - return false
+    @Test
+    public void isNegative_zero_falseReturned() {
+        boolean result = SUT.isNegative(0);
+        assertThat(result, is(false));
+    }
 
 }
